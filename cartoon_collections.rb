2b357669779ec817f06f1b5c55ce072d1ff107e4
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-27 16:37:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,7 @@ def summon_captain_planet (array)
 end
 
 def long_planeteer_calls(array)
-  array.any? do |word|
-    word.length > 4
-  end
+  array.any? { |word| word.length > 4}
 end
 
 def find_the_cheese(array)
