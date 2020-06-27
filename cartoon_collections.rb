5ca497f5fb3e8c128a2a156943bd3af3f6c52e55
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-27 16:41:55 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,17 +10,12 @@ def summon_captain_planet (array)
 end
 
 def long_planeteer_calls(array)
-  i = 0 
-  if array.any? {|i| i.length < 4}
-    return true 
-  else
-    return false 
-    i = i + 1 
-  end
+  array.any? { |word| word.length > 4}
 end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |type| cheese_types.include?(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
 end
